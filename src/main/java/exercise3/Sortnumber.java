3438c753585c2ac63a5da94601ccc0dfc7a241a8
AgathaFlorence <agathaflorencetjhia@outlook.com>
2024-09-04 15:34:06 +0700

    Feat for sort number and fix paragraph

--- src/main/java/exercise3/Sortnumber.java
+++ src/main/java/exercise3/Sortnumber.java
@@ -1,23 +1,27 @@
 package exercise3;
+import java.util.Scanner;
 
 public class Sortnumber {
     public static void sortnumberarray(){//Initialize array
-        int [] arr = new int [] {5, 2, 8, 7, 1};
+
+        int [] arr2 = new int[] {5,6,8,11,1};
+
+
         int temp = 0;
 
         //Displaying elements of original array
         System.out.println("Elements of original array: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
         }
 
         //Sort the array in ascending order
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if(arr[i] > arr[j]) {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = i+1; j < arr2.length; j++) {
+                if(arr2[i] > arr2[j]) {
+                    temp = arr2[i];
+                    arr2[i] = arr2[j];
+                    arr2[j] = temp;
                 }
             }
         }
@@ -26,8 +30,8 @@ public class Sortnumber {
 
         //Displaying elements of array after sorting
         System.out.println("Elements of array sorted in ascending order: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.printf(arr[i] + " ");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.printf(arr2[i] + " ");
         }
 
     }
