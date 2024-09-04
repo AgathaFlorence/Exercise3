ffcdfe536e4f13082e611a23f5e506f5957db7e5
AgathaFlorence <agathaflorencetjhia@outlook.com>
2024-09-04 14:10:39 +0700

    Feat for reversed string but the place must be above the calculations

--- src/main/java/exercise3/Main.java
+++ src/main/java/exercise3/Main.java
@@ -2,6 +2,8 @@ package exercise3;
 
 public class Main {
     public static void main(String[] args) {
+        Reversestring.revstring();
+
     Sumdigit.sumdigitnumber();
 
     Primenumber.isnumberprime();
---
+++ src/main/java/exercise3/Reversestring.java
@@ -0,0 +1,24 @@
+package exercise3;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Reversestring {
+    public static void revstring(){
+        Scanner revword = new Scanner(System.in);
+        System.out.print("Enter the word to be reversed: ");
+        String str = revword.nextLine(), nstr="";
+        char ch;
+
+        System.out.println("Original word: " + str);
+
+
+        for (int i=0; i<str.length(); i++)
+        {
+            ch= str.charAt(i); //extracts each character
+            nstr= ch+nstr; //adds each character in front of the existing string
+        }
+        System.out.println("Reversed word: "+ nstr);
+
+    }
+}
